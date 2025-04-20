bb1e7f714c874538b0317e2d691b4155e2dbb8d7
Weebeezo <peanut_alex24@yahoo.com>
2025-04-20 12:28:00 -0700

    Made it to where it will modify the main file and make its own needed version. Also added a delete function

--- src/main/java/org/DrugTool/FileManagment/ProductFile.java
+++ src/main/java/org/DrugTool/FileManagment/ProductFile.java
@@ -1,25 +1,31 @@
 package org.DrugTool.FileManagment;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Collectors;
 
 public class ProductFile {
+    FileMaster masterFile = new FileMaster();
+    private String fileContent = masterFile.fileContent;
 
-    String fileContent;
-    public ProductFile(Path targetFile) throws IOException {
-        fileContent = Files.readAllLines(targetFile).stream()
-                .collect(Collectors.joining(System.lineSeparator()));
+
+    public ProductFile() throws IOException {
+        int productStart = fileContent.indexOf("DiscoveredProducts");
+        int productFinish = fileContent.indexOf("ListedProducts");
+        fileContent = fileContent.substring(productStart, productFinish);
     }
+
     public String getFileContent(){
         return fileContent;
     }
 
     public String discoveredProducts(String fileContent){
-        int productStart = fileContent.indexOf("DiscoveredProducts");
-        int productFinish = fileContent.indexOf("ListedProducts");
+        int productStart = fileContent.indexOf("[");
+        int productFinish = fileContent.indexOf("]");
 
         return fileContent.substring(productStart, productFinish);
     }
+
+    public void deleteProduct(String productName){
+        fileContent = fileContent.replace(productName, "");
+    }
+
 }
