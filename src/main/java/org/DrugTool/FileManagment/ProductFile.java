66dc7e1e09c48cf64e8fcbd633066fbfb3ca4d83
Weebeezo <peanut_alex24@yahoo.com>
2025-04-20 21:33:59 -0700

    Added some comments for future coders to use and understand

--- src/main/java/org/DrugTool/FileManagment/ProductFile.java
+++ src/main/java/org/DrugTool/FileManagment/ProductFile.java
@@ -8,7 +8,7 @@ public class ProductFile {
     FileMaster masterFile = new FileMaster();
     private String fileContent = masterFile.fileContent;
 
-
+    //Modifies the master file into the necessary Strings by removing everything around it leaving just products
     public ProductFile() throws IOException {
         int productStart = fileContent.indexOf("DiscoveredProducts");
         int productFinish = fileContent.indexOf("ListedProducts");
@@ -18,30 +18,34 @@ public class ProductFile {
     public String getFileContent(){
         return fileContent;
     }
-
+    //Trims the fileContent down a little more to just get the names of products(Might not be a necessary function)
     public String discoveredProducts(String fileContent){
         int productStart = fileContent.indexOf("[");
         int productFinish = fileContent.indexOf("]");
 
         return fileContent.substring(productStart, productFinish);
     }
-
+    //As it says it deletes names of products that is unwanted.
+    //Todo: Try to use a list of string instead of modifing a string as a whole
     public void deleteProduct(String productName){
         fileContent = fileContent.replace("\"" + productName + "\",", "");
     }
+    //Turns fileContent into a List of string with all and only the products stored in for easy use
     public List<String> toList(String fileContent){
         String[] lines = fileContent.split("\\R");
 
         List<String> productList = new ArrayList<>();
         for (String line : lines) {
             String cleaned = line.trim()
-                    .replace(",", "")     // remove comma
-                    .replaceAll("^\"|\"$", ""); // remove leading and trailing quotes
+                    .replace(",", "")
+                    .replaceAll("^\"|\"$", "");
             if (!cleaned.isEmpty()) {
                 productList.add(cleaned);
             }
         }
         return productList;
     }
+    //Todo: Need to add a function that reverts the list back to the string format needed by the game
+
 
 }
